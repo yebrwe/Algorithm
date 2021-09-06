9dc32384ec0f0496ca8840d617acfb3ef8994cd5
unknown <yebrwe@naver.com>
2021-09-07 01:58:39 +0900

    add reversed using stack

--- basic/sentence_reversal.py
+++ basic/sentence_reversal.py
@@ -15,7 +15,7 @@ def reverse_words(str:str) -> str:
       words.append(word)
     else:
       i+=1
-  return " ".join(my_reversed(words))
+  return " ".join(my_reversed_using_stack(words))
 
 from typing import List
 def my_reversed(arr:List[str]) -> List[str]:
@@ -26,5 +26,16 @@ def my_reversed(arr:List[str]) -> List[str]:
     new_arr[i] = arr[length-i-1]
     i+=1
   return new_arr
+
+def my_reversed_using_stack(arr:List[str]) -> List[str]:
+  new_arr = []
+  stack = []
+  for s in arr:
+    stack.append(s)
+  while len(stack) > 0:
+    new_arr.append(stack.pop())
+  return new_arr
+  
+
 
 print(reverse_words("Hello World"))
