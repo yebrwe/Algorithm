f098b90528384686241308edbe525f45f91dfcf4
HoonKyung, Rhee <yebrwe@naver.com>
2021-09-27 15:34:39 +0900

    solving 5397-키로거 occured error: limit time

--- baekjoon/linked_list/5397-키로거.py
+++ baekjoon/linked_list/5397-키로거.py
@@ -3,75 +3,43 @@ class Node:
       self.next = None
       self.prev = None
       self.value = value
-class LinkedList:
-    def __init__(self, arr):
-        self.head = Node(-1)
-        self.tail = Node(-1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.deleted = [0] * 1000000
-        self.deleted_top = -1
-        self.current = self.head
-
-        if not arr: return
-        for o in arr:
-            self.add(o)
-        self.current = self.head
-    def add(self, value) :
-        newNode = Node(value)
-        currentPrevNode = self.current
-        currentNextNode = self.current.next
-        newNode.prev = currentPrevNode
-        newNode.next = currentPrevNode.next
-        currentPrevNode.next = newNode
-        currentNextNode.prev = newNode
-        self.current = newNode
-    def remove(self) :
-        if self.current == self.head or self.current == self.tail: return
-        prevNode = self.current.prev
-        nextNode = self.current.next
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-        self.deleted_top += 1
-        self.deleted[self.deleted_top] = self.current
-        if nextNode == self.tail:
-            self.current = prevNode
-        else:
-            self.current = nextNode
-
-    def restore(self) :
-        if self.deleted_top == -1: return
-        node = self.deleted[self.deleted_top]
-        self.deleted_top -= 1
-        prevNode = node.prev
-        nextNode = node.next
-        prevNode.next = nextNode.prev = node
-
-    def next(self) :
-        if self.current == self.tail: return
-        self.current = self.current.next
-        return self
-
-    def prev(self) :
-        if self.current == self.head: return
-        self.current = self.current.prev
-        return self
+      
 
 n = int(input())
 for _ in range(n):
-  password = input()
-  list = LinkedList([])
-  for c in password:
+  head = Node(-1)
+  tail = Node(-1)
+  head.next = tail
+  tail.prev = head
+  current = head
+
+  for c in input():
     if c == '<':
-      list.prev()
+      if current.prev: current = current.prev
     elif c == '>':
-      list.next()
+      if current.next: current = current.next
     elif c == '-':
-      list.remove()
+      prevNode = current.prev
+      nextNode = current.next
+      if prevNode: prevNode.next = nextNode
+      if nextNode: nextNode.prev = prevNode
     else:
-      list.add(c)
+      newNode = Node(c)      
+      nextNode = current.next
+      
+      current.next = newNode
+      newNode.prev= current
+
+      newNode.next = nextNode
+      if nextNode: nextNode.prev = newNode
+      else: current.prev.next = newNode
+      current = newNode
   
-  node = list.head
-  while list.next:
-    print(list.value)
   
+  result = ''
+  while node != tail:
+    result += node.value
+    node = node.next
+  result += node.value
+  print(result)
+      
