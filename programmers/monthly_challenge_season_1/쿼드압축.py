1a71b3057b69e9f84c76c93b64f2d776b844da7c
HoonKyung, Rhee <yebrwe@naver.com>
2021-10-05 11:14:04 +0900

    쿼드압축 풀기 성공!!

--- programmers/monthly_challenge_season_1/쿼드압축.py
+++ programmers/monthly_challenge_season_1/쿼드압축.py
@@ -1,54 +1,25 @@
 def solution(arr):
-    answer = []
-    n = len(arr)
-    
-    print(arr[0][0:2])
-    print(arr[0:n//2][0:n//2])
-    print(arr[n//2:n][0:n//2])
-    print(arr[0:n//2][n//2:n])
-    print(arr[n//2:n][n//2:n])
-
-    # while n:
-      # print(bfs(arr[0:n//2][0:n//2]))
-      # print(bfs(arr[n//2:n][0:n//2]))
-      # print(bfs(arr[0:n//2][n//2:n]))
-      # print(bfs(arr[n//2:n][n//2:n]))
-      # n //= 2
+    answer = [0, 0]
+    for i in compress(arr):
+        answer[i] += 1
     return answer
 
-
-def bfs(arr):
+def compress(arr): 
     n = len(arr)
-    q = [(0,0)]
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-    only = True
-    count = [0, 0]
-    count[arr[0][0]] += 1
-    visited = [[False] * n for _ in range(n)]
-    while q:
-        x, y = q.pop()
-        if x == n-1 and y == n-1: break
-            
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx<0 or nx>n-1 or ny<0 or ny>n-1: continue
-            if visited[nx][ny]: continue
-            if arr[x][y] != arr[nx][ny]: only = False
-            count[arr[nx][ny]] += 1
-            visited[nx][ny] = True
-            q.insert(0, (nx, ny))
-    if only:
-        return arr[0][0]
-    return count
+    if n == 1:
+        return [arr[0][0]]
+    elif len(set(sum(arr, []))) == 1:
+        return [arr[0][0]]
+    topleft = [ [ arr[i][j] for j in range(n//2)] for i in range(n//2)]
+    topright = [ [ arr[i][j] for j in range(n//2, n)] for i in range(n//2)]
+    bottomleft = [ [ arr[i][j] for j in range(n//2)] for i in range(n//2, n)]
+    bottomright = [ [ arr[i][j] for j in range(n//2, n)] for i in range(n//2, n)]
+    return compress(topleft) + compress(topright) + compress(bottomleft) + compress(bottomright)
 
-
-solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]])
-        
-                
-                
-            
-               
-    
-    
+print(solution(
+    [[1,1,0,0]
+    ,[1,0,0,0]
+    ,[1,0,0,1]
+    ,[1,1,1,1]]
+))
+print(solution([[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]))
