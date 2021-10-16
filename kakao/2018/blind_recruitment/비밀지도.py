4eaf3cebdfdf3a0cea71ef8b4bc00ec16092a93c
yebrwe <yebrwe@naver.com>
2021-10-16 20:45:25 +0900

    비밀지도 품

---
+++ kakao/2018/blind_recruitment/비밀지도.py
@@ -0,0 +1,26 @@
+def solution(n, arr1, arr2):
+    answer = ['' for _ in range(n)]
+    arr1_bin = [to_bin(x, n).replace('0', ' ').replace('1', '#') for x in arr1]
+    arr2_bin = [to_bin(x, n).replace('0', ' ').replace('1', '#') for x in arr2]
+
+
+    for i in range(n):
+        for j in range(n):
+            if arr1_bin[i][j] == '#' or arr2_bin[i][j] == '#':
+                answer[i] += '#'
+            else:
+                answer[i] += ' '
+
+    return answer
+
+def to_bin(n, w):
+    bin = ''
+    while n > 0:
+        div = n // 2
+        mod = n % 2
+        bin += str(mod)
+        n = div
+    return ('0' * (w - len(bin))) + bin[::-1]
+
+
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
