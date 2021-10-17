41c6be50a0b98743fe938b53d97922aa733a22ee
unknown <yebrwe@naver.com>
2021-10-17 19:26:51 +0900

    캐시 품

---
+++ kakao/2018/blind_recruitment/캐시.py
@@ -0,0 +1,24 @@
+from collections import deque
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque([])
+    for city in [city.lower() for city in cities]:
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            answer += 5
+            if len(cache) > 0 and cacheSize == len(cache):
+                cache.popleft()
+            if cacheSize > 0:
+                cache.append(city)
+    return answer
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
+print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju"]))
