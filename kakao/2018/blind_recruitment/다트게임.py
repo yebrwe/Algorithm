cd864fefafcc165347f3e2480905edec8c53202f
HoonKyung, Rhee <yebrwe@naver.com>
2021-10-19 13:55:37 +0900

    다트게임 품

--- kakao/2018/blind_recruitment/다트게임.py
+++ kakao/2018/blind_recruitment/다트게임.py
@@ -17,10 +17,10 @@ def solution(dartResult):
     return sum(rounds, 0)
 
 
-print(solution('1S2D*3T'))
-print(solution('1D2S#10S'))
-print(solution('1D2S0T'))
-print(solution('1S*2T*3S'))
-print(solution('1D#2S*3S'))
-print(solution('1T2D3D#'))
-print(solution('1D2S3T*'))
+# print(solution('1S2D*3T'))
+# print(solution('1D2S#10S'))
+# print(solution('1D2S0T'))
+# print(solution('1S*2T*3S'))
+# print(solution('1D#2S*3S'))
+# print(solution('1T2D3D#'))
+# print(solution('1D2S3T*'))
