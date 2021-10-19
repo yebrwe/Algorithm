fcc5a3d9828b0e99e19770b5128348a5ad352e4c
HoonKyung, Rhee <yebrwe@naver.com>
2021-10-19 13:56:01 +0900

    캐시 품

--- kakao/2018/blind_recruitment/캐시.py
+++ kakao/2018/blind_recruitment/캐시.py
@@ -15,10 +15,10 @@ def solution(cacheSize, cities):
                 cache.append(city)
     return answer
 
-print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
-print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
-print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
-print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
-print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju"]))
+# print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+# print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
+# print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+# print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))
+# print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+# print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+# print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju"]))
