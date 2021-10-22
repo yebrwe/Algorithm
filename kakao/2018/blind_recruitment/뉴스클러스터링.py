e25b08ade39a2e54b7ee3a81ef0bb6cb6740cfc4
HoonKyung, Rhee <yebrwe@naver.com>
2021-10-22 10:56:46 +0900

    뉴스 클러스터링 다른풀이법

--- kakao/2018/blind_recruitment/뉴스클러스터링.py
+++ kakao/2018/blind_recruitment/뉴스클러스터링.py
@@ -9,6 +9,19 @@ def solution(str1, str2):
     if union_len == 0: return 1
     return int(inter_len / union_len * 65536)
 
+def another_solution(str1, str2):
+    str1_multiset = to_multiset(str1.lower())
+    str2_multiset = to_multiset(str2.lower())
+
+    union = set(str1_multiset) | set(str2_multiset)
+    inter = set(str1_multiset) & set(str2_multiset)
+
+    union_len = sum([max(str1_multiset.count(c), str2_multiset.count(c)) for c in union])
+    inter_len = sum([min(str1_multiset.count(c), str2_multiset.count(c)) for c in inter])
+
+    if union_len == 0: return 65536
+    return int(inter_len / union_len * 65536)
+
 def to_multiset(str):
     s = []
     l = len(str)
@@ -22,3 +35,8 @@ print(solution("FRANCE", "french"))
 print(solution("handshake", "shake hands"))
 print(solution("aa1+aa2", "AAAA12"))
 print(solution("E=M*C^2", "e=m*c^2"))
+
+print(another_solution("FRANCE", "french"))
+print(another_solution("handshake", "shake hands"))
+print(another_solution("aa1+aa2", "AAAA12"))
+print(another_solution("E=M*C^2", "e=m*c^2"))
--- kakao/2020/blind_recruitment/자물쇠와열쇠.py
+++ kakao/2020/blind_recruitment/자물쇠와열쇠.py
@@ -1,15 +1,12 @@
-def solution(key, lock):
-    answer = True
-    
-    return answer
+from collections import deque
 
-
-
-arr = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-]
+def reverse_lock(arr):
+    n = len(arr)
+    tmp = [ [0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            tmp[i][j] = 0 if arr[i][j] == 1 else 1
+    return tmp
 
 def move(arr, dir):
     n = len(arr)
@@ -32,17 +29,41 @@ def rotate90(arr):
             tmp[i][j] = arr[n-1-j][i]
     return tmp
 
-def printArray(arr):
-    print("============")
-    n = len(arr)
-    for i in range(n):
-        print(arr[i])
+def open_lock(key, lock, n):
+    q = deque([[key, 0]])
+    while q:
+        [key, level] = q.pop()
+        if level > n // 2: break
+        for i in range(4):
+            new_key = move(key, i)
+            new_key_90 = rotate90(new_key)
+            new_key_180 = rotate90(new_key_90)
+            new_key_270 = rotate90(new_key_180)
+            if lock in [new_key, new_key_90, new_key_180, new_key_270]:
+                return True
+            q.insert(0, [new_key, level+1])
+    return False
 
-printArray(arr)
-arr = rotate90(arr)
-printArray(arr)
-arr = move(arr, 0)
-arr = move(arr, 0)
-arr = move(arr, 0)
-printArray(arr)
+def solution(key, lock):
+    lock = reverse_lock(lock)
+    key_90 = rotate90(key)
+    key_180 = rotate90(key_90)
+    key_270 = rotate90(key_180)
+    if lock in [key, key_90, key_180, key_270]: return True
+    n = len(key)
+    return open_lock(key, lock, n)
 
+
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],	[[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+
+
+
+# print('---key---')
+# for k in key:
+#     print(''.join([str(i) for i in k]))
+# print('---lock---')
+# for l in lock:
+#     print(''.join([str(i) for i in l]))
+# print('---new_key-', level, '---')
+# for k in new_key:
+#     print(''.join([str(i) for i in k]))
