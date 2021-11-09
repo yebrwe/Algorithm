88f24c3d80276ec21d824e4a5905febdf7c6fd0e
HoonKyung, Rhee <yebrwe@naver.com>
2021-11-09 09:29:46 +0900

    프린터 품

--- programmers/stackqueue/프린터.py
+++ programmers/stackqueue/프린터.py
@@ -1,28 +1,14 @@
 def solution(priorities, location):
-    answer = 0
-
-    q = []
-    for i, p in enumerate(priorities):
-        q.insert(0, [i, p])
-
-    cnt = 0
-    while q:
-        max = get_max_priority(q)
-        i, p = q[-1]
-        print(i, q)
-        if q[-1][1] < max:
-            q.insert(0, q.pop())
+    n = len(priorities)
+    waitlist = list(reversed(range(n)))
+    printlist = []
+    while waitlist:
+        J = waitlist.pop()
+        if len(waitlist) > 0 and priorities[J] < max(priorities):
+            waitlist.insert(0, J)
         else:
-            q.pop()
-            cnt += 1
-            if location == i: break
-    print(cnt)
-    return answer
-def get_max_priority(arr):
-    max = -1
-    for i, p in arr:
-        if max < p:
-            max = p
-    return max
-solution([2, 1, 3, 2], 1)
-solution([1, 1, 9, 1, 1, 1], 0)
+            priorities[J] = 0
+            printlist.append(J)
+    return printlist.index(location) + 1
+print(solution([2, 1, 3, 2], 2) == 1)
+print(solution([1, 1, 9, 1, 1, 1], 0) == 5)
