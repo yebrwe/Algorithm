d7432bcdb905b10503b2b77b32919939a6b246ca
HoonKyung, Rhee <yebrwe@naver.com>
2021-11-27 23:27:49 +0900

    네트워크 푸는중

--- programmers/bfs/네트워크.py
+++ programmers/bfs/네트워크.py
@@ -1,10 +1,25 @@
 def solution(n, computers):
-    answer = 0
-    return answer
+    return dfs(n, computers)
 
-def dfs(n, computers, result):
-    network = 0
-    network += dfs()
+def dfs(n, computers):    
+    count = 0
+    visited = [[False] * n for _ in range(n)]
+    for i in range(n):
+        visited[i][i] = True
+    for i in range(n):
+        stack=[i]
+        while stack:
+            i = stack.pop()
+            for j in range(i+1, n):
+                if visited[i][j]: continue
+                visited[i][j] = True
+                if computers[i][j] == 1 and i<j:
+                    stack.append(j)
+                else:
+                    count += 1
+    return count
+
+    
 
 
 print(solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]) ==	2)
