213950051376e1868f8e6fb90b603d1588a224df
yebrwe <yebrwe@naver.com>
2021-11-28 18:14:56 +0900

    네트워크 품

--- programmers/bfs/네트워크.py
+++ programmers/bfs/네트워크.py
@@ -1,26 +1,28 @@
 def solution(n, computers):
-    return dfs(n, computers)
-
-def dfs(n, computers):    
+    graph = [[] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j] == 1:
+                graph[i].append(j)
     count = 0
-    visited = [[False] * n for _ in range(n)]
-    for i in range(n):
-        visited[i][i] = True
-    for i in range(n):
-        stack=[i]
-        while stack:
-            i = stack.pop()
-            for j in range(i+1, n):
-                if visited[i][j]: continue
-                visited[i][j] = True
-                if computers[i][j] == 1 and i<j:
-                    stack.append(j)
-                else:
-                    count += 1
-    return count
+    visited = []
+    while len(visited) < n:
+        dfs(graph, visited)
+        count += 1
+    return count 
 
-    
+def dfs(graph, visited):
+    not_visited = [i for i in range(len(graph)) if i not in visited]
+    stack = [not_visited[0]]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+        waited = [j for j in graph[node] if j not in visited]
+        stack += waited
 
 
-print(solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]) ==	2)
-print(solution(3,	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]) ==	1)
+# print(solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]) ==	2)
+# print(solution(3,	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]) ==	1)
+print(solution(3,	[[1, 0, 0], [0, 1, 0], [0, 0, 1]]) ==	3)
+print(solution(5,	[[1, 1, 0, 0, 0], [1, 1, 1, 0, 0], [0, 1, 1, 1, 0], [0, 0, 1, 1, 0], [0, 0, 0, 0, 1]]) ==	2)
