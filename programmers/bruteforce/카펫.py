d5d446b1c09f0e1d1f993fea0ea2be7b5aea3105
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-02 00:31:32 +0900

    카펫 품

--- programmers/bruteforce/카펫.py
+++ programmers/bruteforce/카펫.py
@@ -1,24 +1,16 @@
 def solution(brown, yellow):
-    answer = []
+    count = brown+yellow
     sqrt_yellow = yellow**0.5
     #제곱
     if sqrt_yellow == int(sqrt_yellow):
-      return [yellow+2, yellow+2]
-    else:
-      w = yellow+2
-      h = 3
-      #막대기
-      if w*h == brown+yellow:
-        answer = [w,h]
-      #직사각형
-      else:
-        count = brown+yellow
-        for i in range(count):
-          w, h = i+1, int(count // (i+1))
-          if w * h == count and (w-2) * (h-2) == yellow and w>h:
-            answer = [w, h]
-            break
-    return answer
+      return [sqrt_yellow+2, sqrt_yellow+2]
+    #그외
+    for i in range(count):
+      w, h = i+1, int(count // (i+1))
+      if w * h == count and (w-2) * (h-2) == yellow and w>h:
+        return[w, h]
+    return []
 print(solution(10,	2))
 print(solution(8,	1))
 print(solution(24,	24))
+print(solution(12,	4))
