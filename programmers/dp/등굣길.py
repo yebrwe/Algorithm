ce268112ed31c13aa320c643ca0a3f2a62456712
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-07 23:09:49 +0900

    등굣길 품

---
+++ programmers/dp/등굣길.py
@@ -0,0 +1,11 @@
+def solution(m, n, puddles):
+  roads = [[0]*m for _ in range(n)]
+  roads[0][0]= 1
+  for i in range(n):
+    for j in range(m):
+      if i==0 and j==0:continue
+      if [i+1,j+1] in puddles: continue
+      roads[i][j] = roads[i-1][j] + roads[i][j-1]
+  return roads[n-1][m-1] % 1000000007
+print(solution(4,	3,	[[2, 2]]) ==	4)
+
