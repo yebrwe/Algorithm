61815a50bbb71b16507db9f8db83b18602854d6a
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-23 15:11:16 +0900

    주사위게임 품

---
+++ inflearn/코드 구현력 기르기/9. 주사위 게임.py
@@ -0,0 +1,19 @@
+n=int(input())
+res=0
+for _ in range(n):
+    tmp=input().split()
+    tmp.sort()
+    a,b,c=map(int, tmp)
+    money = 0
+    if a==b and b==c:
+        money = 10000+a*1000
+    elif a==b or a==c:
+        money = 1000+a*100
+    elif b==c:
+        money = 1000+b*100
+    else:
+        money = c*100
+    
+    if money > res:
+        res=money
+print(res)
