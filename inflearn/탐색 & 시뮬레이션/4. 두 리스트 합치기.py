569539a63c886c8318d53f56d9ec92bf0bcd25b3
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-23 18:03:37 +0900

    두 리스트 합치기 품

---
+++ inflearn/탐색 & 시뮬레이션/4. 두 리스트 합치기.py
@@ -0,0 +1,19 @@
+n=int(input())
+a=list(map(int, input().split()))
+m=int(input())
+b=list(map(int, input().split()))
+p1=p2=0
+c=[]
+while p1!=n and p2!=m:
+    if a[p1] < b[p2]:
+        c.append(a[p1])
+        p1+=1
+    else:
+        c.append(b[p2])
+        p2+=1
+if p1<n:
+    c+=a[p1:]
+if p2<m:
+    c+=b[p2:]
+for x in c:
+    print(x, end=" ")
