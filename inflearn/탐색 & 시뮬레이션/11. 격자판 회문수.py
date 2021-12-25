f32ef84500e0400579a34ffc2587a8106485715d
yebrwe <yebrwe@naver.com>
2021-12-25 11:25:47 +0900

    격자판 회문수 품

--- inflearn/탐색 & 시뮬레이션/11. 격자판 회문수.py
+++ inflearn/탐색 & 시뮬레이션/11. 격자판 회문수.py
@@ -1,19 +1,20 @@
-import sys
-sys.stdin=open(r"./inflearn/input.txt", "rt")
 a=[list(map(int, input().split())) for _ in range(7)]
 res=0
-for i in range(3):
+for i in range(7):
     for j in range(3):
-        sx=i
-        ex=7-3+i
-        sy=j
-        ey=7-3+j
+        sx=j
+        ex=7-3+j
         for k in range(3):
-            if a[sx][sy+k] != a[sx][ey-k]:break
+            if a[i][sx+k] != a[i][ex-k]:break
         else:
             res+=1
+
+for i in range(3):
+    for j in range(7):
+        sy=i
+        ey=7-3+i
         for s in range(3):
-            if a[sx+s][sy] != a[ex-s][sy]:break
+            if a[sy+s][j] != a[ey-s][j]:break
         else:
             res+=1
 print(res)
