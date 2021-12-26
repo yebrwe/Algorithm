a6c3ed88ce2ff8cd39109dd4e3c628a91907c7b1
HoonKyung, Rhee <yebrwe@naver.com>
2021-12-26 21:26:02 +0900

    모범답안으로 다시품

--- inflearn/탐색 & 시뮬레이션/11. 격자판 회문수.py
+++ inflearn/탐색 & 시뮬레이션/11. 격자판 회문수.py
@@ -1,22 +1,14 @@
 a=[list(map(int, input().split())) for _ in range(7)]
-res=0
-for i in range(7):
-    for j in range(3):
-        sx=j
-        ex=7-3+j
-        for k in range(3):
-            if a[i][sx+k] != a[i][ex-k]:break
-        else:
-            res+=1
-
+cnt=0
 for i in range(3):
     for j in range(7):
-        sy=i
-        ey=7-3+i
-        for s in range(3):
-            if a[sy+s][j] != a[ey-s][j]:break
-        else:
-            res+=1
-print(res)
+        tmp=a[j][i:i+5]
+        if tmp==tmp[::-1]:
+            cnt+=1
 
-                
+        for k in range(2):
+            if a[i+k][j] != a[5+i-k-1][j]:
+                break
+        else:
+            cnt+=1
+print(cnt)
