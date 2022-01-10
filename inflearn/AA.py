4561fb49dab18a5638f418e5a1211922ebd4c502
HoonKyung, Rhee <yebrwe@naver.com>
2022-01-10 21:43:29 +0900

    K번째 큰 수 품

--- inflearn/AA.py
+++ inflearn/AA.py
@@ -1,13 +1,12 @@
 import sys
 sys.stdin=open(r"./inflearn/input.txt", "rt")
-n,k=map(int, input().split())
+n, k=map(int, input().split())
 a=list(map(int, input().split()))
 res=set()
 for i in range(n):
     for j in range(i+1, n):
         for m in range(j+1, n):
-            res.add(a[i] + a[j] + a[m])
+            res.add(a[i]+a[j]+a[m])
 res=list(res)
 res.sort(reverse=True)
 print(res[k-1])
-    
