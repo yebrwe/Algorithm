e2a988ff94cc2dc8c1d51e1070c2ce4d9a4d1f5c
HoonKyung, Rhee <yebrwe@naver.com>
2022-01-13 12:30:25 +0900

    메뉴리뉴얼 품!!

--- kakao/2021/blind_recruitment/메뉴리뉴얼.py
+++ kakao/2021/blind_recruitment/메뉴리뉴얼.py
@@ -1,11 +1,27 @@
+comb=['']*10
 def solution(orders, course):
-    for order in orders:
-        print(order)
+    answer = []
+    for c in course:
+        res=dict()
+        for o in orders:
+            DFS(0, 0, o, c, res)
+        max=2
+        for x in res:
+            if res[x]>=max:
+                max=res[x]
+        for x in res:
+            if max==res[x]:
+                answer.append(x)
+    answer.sort()
+    return answer
 
-
-
-
-		
-# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]) == ["AC", "ACDE", "BCFG", "CDE"])
-# print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"],[2,3,5]) == ["ACD", "AD", "ADE", "CD", "XYZ"])
-# print(solution(["XYZ", "XWY", "WXA"],[2,3,4]) == ["WX", "XY"])
+def DFS(L, s, o, c, res):
+    if L==c:        
+        key=''.join(sorted(list(''.join(comb))))
+        res[key]=res.get(key, 0) + 1
+    else:
+        for i in range(s, len(o)):
+            comb[L]=o[i]
+            DFS(L+1, i+1, o, c, res)            
+        
+    
